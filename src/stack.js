bf0f08f6961c26a5dc6b00c737c8a2c6e173497b
wickedside <w1ck3ds1d3@gmail.com>
2022-12-13 20:19:18 +0300

    feat: Stack push

--- src/stack.js
+++ src/stack.js
@@ -13,9 +13,9 @@ const { NotImplementedError } = require("../extensions/index.js");
  *
  */
 module.exports = class Stack {
-  push(/* element */) {
-    throw new NotImplementedError("Not implemented");
-    // remove line with error and write your code here
+  #array = [];
+  push(element) {
+    return this.#array.push(element);
   }
 
   pop() {
